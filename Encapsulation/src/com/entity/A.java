2fa08e08ef96d8b7f60be45f8f680d25fadc76c6
sunita123github <sunitawag199@gmail.com>
2023-04-27 17:28:28 +0530

    first commit

---
+++ Encapsulation/src/com/entity/A.java
@@ -0,0 +1,13 @@
+package com.entity;
+
+public class A {
+private int salary;
+
+public int getSalary() {
+	return salary;
+}
+
+public void setSalary(int salary) {
+	this.salary = salary;
+}
+}
---
+++ Encapsulation/src/com/entity/C.java
@@ -0,0 +1,17 @@
+package com.entity;
+
+public class C {
+
+	public static void main(String[] args) {
+		B b = new B();
+		b.setName("sunita");
+		b.setId(225);
+		System.out.println("your Name is " + b.getName());
+		System.out.println("your id is " + b.getId());
+		
+		
+		
+
+	}
+
+}
