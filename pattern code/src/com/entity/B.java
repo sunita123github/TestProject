61bd0d67e1f660681921d31c5569e27a509925ca
sunita123github <sunitawag199@gmail.com>
2023-04-27 17:30:34 +0530

    first commit

---
+++ pattern code/src/com/entity/B.java
@@ -0,0 +1,19 @@
+package com.entity;
+
+public class B {
+
+	public static void main(String[] args) {
+	for (int i = 1; i<=4; i++) {
+		for(int j = 4; j>=i; j--) {
+			System.out.print("*");
+			
+		}
+		System.out.println();
+		
+	}
+	
+	
+
+	}
+
+}
